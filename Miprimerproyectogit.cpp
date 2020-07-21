2780da44f6e13ff7d16ad21dc558713995b5ed72
Ivan Chavez <ivanchavezduarte@gmail.com>
2020-07-20 21:12:31 -0700

    Cambios al codigo para elimitar una libreria inecesaria y comentar el codigo+

---
+++ Miprimerproyectogit.cpp
@@ -0,0 +1,14 @@
+/*
+Este es mi primer programa el cual hago commit para Git 
+NOTA: Estoy siguiendo el curso de Git y Git Hub en Platzi con Freddy Vega!
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hola Mama estoy programando! lml" << endl;
+    // Enciende la radio!
+    return 0;
+}
